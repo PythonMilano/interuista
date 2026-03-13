35662b95eb5ab663010b71f165af4e06b0d70303
Cesare Placanica <1519356+keobox@users.noreply.github.com>
2026-03-13 08:21:13 +0100

    Fix SITEURL

--- publishconf.py
+++ publishconf.py
@@ -4,5 +4,5 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *  # noqa: E402, F401, F403
 
-SITEURL = "https://milano.python.it/interuista"
+SITEURL = "https://www.intervistapythonista.com"
 RELATIVE_URLS = False
